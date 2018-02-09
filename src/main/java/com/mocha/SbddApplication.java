415608ffc6c3c5fb3341a885c800678071e35f70
zhenfeng-zhu <codeless98@163.com>
2018-02-09 14:55:34 +0800

    Initial commit

---
+++ src/main/java/com/mocha/SbddApplication.java
@@ -0,0 +1,12 @@
+package com.mocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbddApplication.class, args);
+	}
+}
